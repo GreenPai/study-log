287ee6556d0657f468513adfab533fead9073d1d
roberty <clsrntkdgh@gmail.com>
2025-07-09 21:03:00 +0900

    스프링부트 백엔드 개발 입문

---
+++ 혼코딩/honco/src/main/java/com/greenpai/honco/HoncoApplication.java
@@ -0,0 +1,13 @@
+package com.greenpai.honco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoncoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoncoApplication.class, args);
+    }
+
+}
---
+++ 혼코딩/honco/src/test/java/com/greenpai/honco/HoncoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenpai.honco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoncoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
